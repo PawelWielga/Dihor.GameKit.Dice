73f806c25eb2ad9bac9e38eeff5d8a883d9f8dba
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-17 12:38:59 +0200

    feat: export playback appearance options

--- src/player/index.ts
+++ src/player/index.ts
@@ -6,6 +6,7 @@ export type {
   DiceAnimationScheduler,
   DiceRenderTarget,
   DiceRollPlaybackDieResult,
+  DiceRollPlaybackOptions,
   DiceRollPlaybackResult,
   DiceRollPlayerOptions
 } from "./DiceRollPlayer.js";
