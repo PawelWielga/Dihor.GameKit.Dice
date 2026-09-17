eb7d06c3565cbfa7bdd92284ba4545c274725168
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-17 13:42:27 +0200

    Export seeded random provider

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,6 +5,11 @@ export type { DiceRollRequest } from "./DiceRollRequest.js";
 export type { DiceRollResult } from "./DiceRollResult.js";
 export type { DieResult } from "./DieResult.js";
 export type { RandomProvider } from "./RandomProvider.js";
+export {
+  SeededRandomProvider,
+  createSeededRandomProvider
+} from "./SeededRandomProvider.js";
+export type { RandomSeed } from "./SeededRandomProvider.js";
 export { SUPPORTED_DICE_SIDES } from "./DiceSides.js";
 export type { DiceSides } from "./DiceSides.js";
 export {
