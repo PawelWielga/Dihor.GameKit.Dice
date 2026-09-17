2b2355777b2abd830ff7453014fc917702b081d1
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-17 13:47:55 +0200

    Export multiplayer event contract

---
+++ src/events/index.ts
@@ -0,0 +1,13 @@
+export {
+  DICE_ROLL_EVENT_TYPE,
+  DICE_ROLL_EVENT_VERSION,
+  DICE_ROLL_REPLAY_VERSION,
+  createDiceRollEvent,
+  diceRollResultFromEvent
+} from "./DiceRollEvent.js";
+export type {
+  CreateDiceRollEventOptions,
+  DiceRollEvent,
+  DiceRollEventDie,
+  DiceRollReplayV1
+} from "./DiceRollEvent.js";
