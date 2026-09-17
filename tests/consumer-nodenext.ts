175f454ff12e8b4a549f0b7ccbb6aa19963f4ca4
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-17 14:04:16 +0200

    Exercise multiplayer event exports in consumer test

--- tests/consumer-nodenext.ts
+++ tests/consumer-nodenext.ts
@@ -1,5 +1,7 @@
 import {
   SUPPORTED_DICE_SIDES,
+  createDiceRollEvent,
+  validateDiceRollEvent,
   type DiceAppearance,
   type DiceRollRequest
 } from "@partybeam/dice-kit";
@@ -19,6 +21,14 @@ const request: DiceRollRequest = {
 
 const appearance: DiceAppearance = request.dice[0]?.appearance ?? {};
 const missingFaceTexture: string | undefined = appearance.faces?.[1];
+const event = createDiceRollEvent({
+  rollId: "consumer-roll",
+  dice: [{ sides: 20, value: 20 }],
+  modifier: 0,
+  total: 20
+}, { definitions: request.dice });
+const validatedEvent = validateDiceRollEvent(JSON.parse(JSON.stringify(event)));
 
 void SUPPORTED_DICE_SIDES;
 void missingFaceTexture;
+void validatedEvent;
