386008ce4b1028b6e011bf022440eee9c8e170ee
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-19 10:37:54 +0200

    Expose replay v2 from advanced API

--- src/advanced.ts
+++ src/advanced.ts
@@ -56,6 +56,7 @@ export {
   DICE_ROLL_EVENT_LIMITS,
   DICE_ROLL_EVENT_TYPE,
   DICE_ROLL_EVENT_VERSION,
+  DICE_ROLL_REPLAY_V1_VERSION,
   DICE_ROLL_REPLAY_VERSION,
   createDiceRollEvent,
   diceRollResultFromEvent,
@@ -68,7 +69,9 @@ export {
   type DiceRollEventAssetPolicy,
   type DiceRollEvent,
   type DiceRollEventDie,
-  type DiceRollReplayV1
+  type DiceRollReplay,
+  type DiceRollReplayV1,
+  type DiceRollReplayV2
 } from "./events/index.js";
 export {
   DiceOverlay,
